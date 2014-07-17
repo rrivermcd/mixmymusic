ece9a53a5d0cc903a7a7308e2b2a1cc48e883822
Michael McDermott <rrivermcd@cox.net>
2014-07-17 11:50:51 -0400

    fixed following users

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,6 @@
 ﻿class MicropostsController < ApplicationController
-  before_action :signed_in_user, only: [create, :destroy]
-  before_action :correct_user,   only: :destroy  
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -12,10 +12,10 @@
       render 'static_pages/home'
     end
   end
-  
+
   def destroy
-  	@micropost.destroy
-  	redirect_to root_url
+    @micropost.destroy
+    redirect_to root_url
   end
 
   private
@@ -23,9 +23,9 @@
     def micropost_params
       params.require(:micropost).permit(:content)
     end
-    
+
     def correct_user
-    	@micropost = current_user.microposts.find_by(id: params[:id])
-    	redirect_to root_url if @micropost.nil?
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
--- app/controllers/mix_my_music_controller.rb
+++ app/controllers/mix_my_music_controller.rb
@@ -1,18 +1,18 @@
 class MixMyMusicController < ApplicationController
   
   def home
-	if signed_in?  
-		@micropost = current_user.microposts.build if signed_in?
-	  	@feed_items = current_user.feed.paginate(page: params[:page])
-  	end
+    if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
-  
+
   def help
   end
-  
+
   def about
   end
-  
+
   def contact
   end
 end
