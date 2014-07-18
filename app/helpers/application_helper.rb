642c047bf70b4c7dd807161152958181cf3145bc
Michael McDermott <rrivermcd@cox.net>
2014-07-18 09:56:39 -0400

    changed name to MMM

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
 	# Returns the full title on a per-page basis.
 	def full_title(page_title)
-		base_title = "Mix My Music"
+		base_title = "MMM"
 		if page_title.empty?
 			base_title
 		else
