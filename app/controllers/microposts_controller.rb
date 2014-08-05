4ec40b5ba0d7c1d1aa6de80c789459b766a65889
Michael McDermott <rrivermcd@cox.net>
2014-08-05 14:22:30 -0400

    mostly housekeeping :)

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,7 @@
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = "It's been Shouted!"
       redirect_to root_url
     else
       @feed_items = []
