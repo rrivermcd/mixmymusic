7b9a42c6ba30bc2f2c9165cc96a2fae588561e75
Michael McDermott <rrivermcd@cox.net>
2014-09-22 15:38:58 -0400

    alternate tracks

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -9,17 +9,58 @@ module TracksHelper
 		@bucket = s3.buckets['mixmymusic']
 	end
 
+	def first_by_role(track, song)
+		if song.tracks.length > 1
+			first = false
+			role_exists = false
+			@alternatives = []
+			track_roles = pluck_role_by_id(track.id, song.id)
+			song_part = pluck_part(track.id, song.id)
+			sister_parts = Part.where(song_id: song.id).where.not(track_id: track.id)
+			sister_parts.each do |sister_part|
+		 		sister_part_roles = sister_part.roles.pluck(:id)
+		 		if !(track_roles & sister_part_roles).empty?
+		 			role_exists = true
+		 			if song_part.id < sister_part.id
+		 				first = true
+		 				@alternatives.push(Track.find_by(id: sister_part.track_id))
+		 			end
+		 		end	
+			end
+		 	if !role_exists
+		 		first = true
+		 	end
+		 else
+		 	first = true
+		end
+		return first
+	end
+
+
+	def pluck_part(track_id, song_id)
+		Part.find_by(song_id: song_id, track_id: track_id)
+	end
+
 	def pluck_role(track_id, song_id)
-		part = Part.find_by(song_id: song_id, track_id: track_id)
+		# part = Part.find_by(song_id: song_id, track_id: track_id)
+		part = pluck_part(track_id, song_id)
 		roles = part.roles.pluck(:role)
-		roles.join (',')
+		# roles.join (',')
+	end
+
+	def pluck_role_by_id(track_id, song_id)
+		part = pluck_part(track_id, song_id)
+		roles = part.roles.pluck(:id)
+	end
+
+	def pluck_role_as_string(track_id, song_id)
+		roles = pluck_role(track_id, song_id)
+		roles.join(',')
 	end
 
 	def is_audio(track_url)
 		File.extname(track_url) != '.pdf'
 	end
-
-
 end
 
 
