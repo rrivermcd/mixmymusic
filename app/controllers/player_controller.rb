357ef7546bffe55b60bc592aaf6310dd6981b337
Michael McDermott <rrivermcd@cox.net>
2014-09-24 10:20:47 -0400

    remove shared player

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -1,7 +1,7 @@
 class player < ApplicationController
 
 	def show
-		@s3 = AWS::S3.new
-		@bucket = s3.buckets['mixmymusic'] 
-		render 'shared/player'
+		# @s3 = AWS::S3.new
+		# @bucket = s3.buckets['mixmymusic'] 
+		# render 'shared/player'
 	end
